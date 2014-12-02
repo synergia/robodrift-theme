63c736a0a24ca23efc83861ef105d0449567e5bf
stsdc <s.dac@yandex.ru>
2014-12-02 20:53:34 +0100

    Tytuł rejestracji

--- template-part-rejestracja.php
+++ template-part-rejestracja.php
@@ -1,6 +1,8 @@
 <section id="rejestracja" class="main">
     <header>
         <div class="container">
+            <h2>Rejestracja pojazdu</h2>
+            <br>
             <a href="<?php get_bloginfo('url');?>/rejestracja" target="_blank" class="button">Zarejestruj się</a>
 
 
